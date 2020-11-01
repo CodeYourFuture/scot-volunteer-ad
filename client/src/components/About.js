906660ef6b82057649be1a579c9b90ecc0a75ff1
Awet <awetkifle94@gmail.com>
2020-11-01 13:53:49 +0000

    changed title of about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About </h1>
+      <p> This is my about page for volunterr app project.</p>
+    </div>
+  );
 };
 
 export default About;
