795b68ddf92b74cc9a145fba5cfa432294319f5d
RAZAN <razan@87654321@gmail.com>
2020-11-18 21:37:09 +0000

    please checking what the problem with this code

---
+++ server/services/database/assessments.js
@@ -0,0 +1,34 @@
+const { Pool } = require("pg");
+const config = require("../../config");
+const pool = new Pool(config);
+
+
+const getAllAssessments = () => {
+    return pool.query("select * from assessments")
+    .then((result) => result.rows);
+};
+
+const getAssessmentById=(id)=> {
+
+   return pool
+    .query("SELECT * FROM assessments WHERE assessment_id=$1", [id])
+    .then((result) => result.rows[0]);
+    
+   
+}
+const createNewAssessment=( topicId, newAssessment)=> {
+ 
+  const query = "INSERT INTO assessments (topic_id, assessment_name) VALUES ($1,$2)";
+    
+    return pool
+        .query(query, [ topicId, newAssessment ])
+        
+    
+            };
+
+  
+module.exports = {
+    getAllAssessments,
+    getAssessmentById,
+    createNewAssessment,
+};
