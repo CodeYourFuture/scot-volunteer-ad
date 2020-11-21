ac10ce0951d2d8ea3e71dca391908668cea6cc1d
RAZAN <razan@87654321@gmail.com>
2020-11-21 00:50:05 +0000

    create new 3 files that deleted before contain all endpoints for the assessment file

---
+++ server/api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const users = require("./users");
+const status = require("./status");
+const assessments = require("./assessments");
+
+router.use("/users", users);
+router.use("/status", status);
+router.use("/assessments",assessments);
+
+module.exports = router;
---
+++ server/services/database/assessments.js
@@ -0,0 +1,38 @@
+const { Pool } = require("pg");
+const config = require("../../config");
+const pool = new Pool(config);
+
+
+const getAllAssessments = () => {
+    return pool.query("select * from assessments")
+    .then((result) => result.rows);
+};
+
+const getAssessmentById=(id)=> {
+
+   return pool
+    .query("SELECT * FROM assessments WHERE assessment_id=$1", [id])
+    .then((result) => result.rows[0]);
+    
+   
+}
+const createNewAssessment=( topicId, newAssessment)=> {
+ 
+  const query = "INSERT INTO assessments (topic_id, assessment_name) VALUES ($1,$2)";
+    
+    return pool
+        .query(query, [ topicId, newAssessment ])
+            };
+
+            const getAllQuestions = (id) => {
+                
+                return pool.query("select * from questions where assessment_id = $1" ,[id])
+                .then((result) => result.rows);
+            };
+            
+  
+module.exports = {
+    getAllAssessments,
+    getAssessmentById,
+    createNewAssessment,
+    getAllQuestions };
